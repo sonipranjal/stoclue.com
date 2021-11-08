99c2e2c497848e5a5c51b47ee9da453c0f63cc41
sonipranjal <pranjalsoni6256@gmail.com>
2021-11-08 15:42:00 +0530

    fixed apis typos

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   images: {
-    domains: ["images.unsplash.com", "images.pexels.com", "image.freepik.com"],
+    domains: ['images.unsplash.com', 'images.pexels.com', 'image.freepik.com'],
   },
   env: {
-    BASE_URL=process.env.BASE_URL,
-    NEXT_PUBLIC_BASE_URL=process.env.NEXT_PUBLIC_BASE_URL,
-    MAIL_SERVER_PASSWORD=process.env.MAIL_SERVER_PASSWORD,
-    MAIL_SERVER_USERNAME=process.env.MAIL_SERVER_USERNAME,
-    NEXT_PUBLIC_SECRET_KEY=process.env.NEXT_PUBLIC_SECRET_KEY,
-    SECRET_KEY=process.env.SECRET_KEY
-  }
+    BASE_URL: process.env.BASE_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    MAIL_SERVER_PASSWORD: process.env.MAIL_SERVER_PASSWORD,
+    MAIL_SERVER_USERNAME: process.env.MAIL_SERVER_USERNAME,
+    NEXT_PUBLIC_SECRET_KEY: process.env.NEXT_PUBLIC_SECRET_KEY,
+    SECRET_KEY: process.env.SECRET_KEY,
+  },
 };
